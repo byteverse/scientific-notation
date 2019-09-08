2865895e0190e688f5b06d279f3ec7cf0b600f4b
Andrew Martin <andrew.thaddeus@gmail.com>
2019-09-08 09:31:43 -0400

    benchmark integer conversion against scientific package

--- bench/Main.hs
+++ bench/Main.hs
@@ -1,12 +1,15 @@
 {-# language BangPatterns #-}
 {-# language PackageImports #-}
 {-# language MagicHash #-}
+{-# language ScopedTypeVariables #-}
 
 import Gauge (bgroup,bench,whnf)
 import Gauge.Main (defaultMain)
 import Data.ByteString.Internal (ByteString(PS))
-import Data.Primitive (SmallArray,ByteArray(..))
+import Data.Primitive (SmallArray,PrimArray,ByteArray(..))
+import Data.Word (Word16)
 import Control.Monad.ST (runST)
+import Control.Monad.ST.Run (runPrimArrayST)
 import GHC.ForeignPtr (ForeignPtrContents(PlainPtr))
 import GHC.ForeignPtr (ForeignPtr(ForeignPtr))
 
@@ -22,12 +25,23 @@ import qualified "scientific-notation" Data.Number.Scientific as SCI
 
 main :: IO ()
 main = defaultMain
-  [ bgroup "parsing"
-    [ bench "ten-small" (whnf (\b -> P.parseByteArray decodeTen b) tenSmall)
+  [ bgroup "scientific-notation"
+    [ bgroup "parser"
+      [ bench "ten-small"
+          (whnf (\b -> P.parseByteArray decodeTen b) tenSmall)
+      ]
+    , bgroup "conversion"
+      [ bench "twenty-word16"
+        (whnf (\b -> convertArray16 b) twentyFastSci)
+      ]
     ]
-  , bgroup "aeson"
+  , bgroup "scientific"
     [ bench "ten-small"
       (whnf (\b -> Atto.parseOnly (aesonDecodeN 10 []) (fromPinned b)) tenSmall)
+    , bgroup "conversion"
+      [ bench "twenty-word16"
+        (whnf (\b -> convertSlowArray16 b) twentySlowSci)
+      ]
     ]
   ]
 
@@ -47,6 +61,39 @@ tenSmall = pin $ Bytes.toByteArray $ Bytes.fromAsciiString $ concat
   , ",0000.002"
   , ",0000.002E1"
   ]
+
+-- All of these can fit inside a Word16.
+twentyPairs :: SmallArray (Int,Int)
+twentyPairs = Exts.fromList
+  [ (2336,0)
+  , (43265,0)
+  , (17,0)
+  , (24,3)
+  , (1,4)
+  , (25,0)
+  , (0,0)
+  , (1900,0)
+  , (65,0)
+  , (1100,0)
+  , (5,3)
+  , (0,0)
+  , (1600,0)
+  , (1500,0)
+  , (2000,0)
+  , (62,2)
+  , (500,0)
+  , (670,0)
+  , (1100,0)
+  , (65500,0)
+  ]
+
+twentyFastSci :: SmallArray SCI.Scientific
+twentyFastSci = fmap (uncurry SCI.small) twentyPairs
+
+twentySlowSci :: SmallArray SlowSci.Scientific
+twentySlowSci = fmap
+  (\(x,y) -> SlowSci.scientific (fromIntegral x) y)
+  twentyPairs
 
 aesonDecodeN :: Int -> [SlowSci.Scientific] -> Atto.Parser [SlowSci.Scientific]
 aesonDecodeN !ix !acc = if ix > 0
@@ -68,6 +115,36 @@ decodeTen = do
         else P.effect (PM.unsafeFreezeSmallArray arr)
   go 9
 
+convertArray16 ::
+     SmallArray SCI.Scientific
+  -> PrimArray Word16
+convertArray16 xs = runPrimArrayST $ do
+  let len = PM.sizeofSmallArray xs
+  ws <- PM.newPrimArray len
+  let go !ix = if ix >= 0
+        then case SCI.toWord16 (PM.indexSmallArray xs ix) of
+          Nothing -> error "convertArray16: bad number"
+          Just (r :: Word16) -> do
+            PM.writePrimArray ws ix r
+            go (ix - 1)
+        else PM.unsafeFreezePrimArray ws
+  go (len - 1)
+
+convertSlowArray16 ::
+     SmallArray SlowSci.Scientific
+  -> PrimArray Word16
+convertSlowArray16 xs = runPrimArrayST $ do
+  let len = PM.sizeofSmallArray xs
+  ws <- PM.newPrimArray len
+  let go !ix = if ix >= 0
+        then case SlowSci.toBoundedInteger (PM.indexSmallArray xs ix) of
+          Nothing -> error "convertArray16: bad number"
+          Just (r :: Word16) -> do
+            PM.writePrimArray ws ix r
+            go (ix - 1)
+        else PM.unsafeFreezePrimArray ws
+  go (len - 1)
+
 errorThunk :: a
 {-# noinline errorThunk #-}
 errorThunk = error "scientific:benchmark error"
@@ -85,4 +162,3 @@ pin src = runST $ do
   dst <- PM.newByteArray len
   PM.copyByteArray dst 0 src 0 len
   PM.unsafeFreezeByteArray dst
-
